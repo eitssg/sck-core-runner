4fbe658565adb1802d157e47939da0b8d90de680
James Barwick <jbarwick@me.com>
2024-12-24 18:52:24 +0800

    Update Docs

--- core_runner/handler.py
+++ core_runner/handler.py
@@ -1,5 +1,5 @@
-# Description: Executes the action runner step function
-#
+"""Executes the action runner step function"""
+
 import core_logging as log
 import time
 
@@ -8,16 +8,42 @@ import core_helper.aws as aws
 import core_framework as util
 from core_framework.models import TaskPayload
 
-from core_execute.stepfn import local_step_function_client
+import core_execute.stepfn as local
 
 
 def handler(event: dict, context: dict | None) -> dict:
+    """
+    Executes the action runner step function.
+
+    This lambda function expects a TaskPayload object as input and executes the action runner step function.
+
+    The TaskPayload is validated and a unique execution name is generated based on the TaskPayload details.
+
+    The return value is the following dictionary:
+
+    .. code-block:: json
+        {
+           "Status": "ok | error",
+          "Message": "Executed step function '<execution_arn>' | Failed to execute step function",
+          "StepFunctionInput": { "task": "... the task payload ..." },
+          "ExecutionArn": "<execution_arn>"
+        }
+
+    Args:
+        event (dict): The task payload object
+        context (dict | None): The context of the execution
+
+    Returns:
+        dict: The result of the execution
+    """
     try:
         # event should have been created with TaskPayload.model_dump()
         task_payload = TaskPayload(**event)
 
         # Setup logging
         log.setup(task_payload.Identity)
+
+        log.info("Executing step function", details=task_payload.model_dump())
 
         name = generate_execution_name(task_payload)
 
@@ -33,12 +59,16 @@ def handler(event: dict, context: dict | None) -> dict:
             details={"ExecutionArn": execution_arn},
         )
 
-        return {
+        result = {
             "Status": "ok",
             "Message": "Executed step function '{}'".format(execution_arn),
             "StepFunctionInput": task_payload.model_dump(),
             "ExecutionArn": execution_arn,
         }
+
+        log.debug("Result", details=result)
+
+        return result
 
     except Exception as e:
         log.error("Failed to execute step function", details={"Error": str(e)})
@@ -50,7 +80,17 @@ def handler(event: dict, context: dict | None) -> dict:
 
 
 def lambda_start_execution(task_payload: TaskPayload, name: str) -> dict:
+    """
+    Start the execution of a step function in AWS. This will start the step function
+    in the AWS Step Functions service.
 
+    Args:
+        task_payload (TaskPayload): The task payload to execute
+        name (str): The name of the execution
+
+    Returns:
+        dict: Result of the job start request
+    """
     region = util.get_region()
     arn = util.get_step_function_arn()
     data = task_payload.model_dump_json()
@@ -65,7 +105,18 @@ def lambda_start_execution(task_payload: TaskPayload, name: str) -> dict:
 
 
 def local_start_execution(task_payload: TaskPayload, name: str) -> dict:
+    """
+    Start the execution of a step function in local mode. This means it will
+    run in a shell process in the background and continue to run until it fails,
+    completes, or is stopped by the OS.
 
+    Args:
+        task_payload (TaskPayload): The task payload to execute
+        name (str): the executin name
+
+    Returns:
+        dict: Result of the job start request
+    """
     region = util.get_region()
     arn = f"arn:aws:states:{region}:local:execution:stateMachineName:{name}"
 
@@ -76,11 +127,33 @@ def local_start_execution(task_payload: TaskPayload, name: str) -> dict:
         details={"StepFunctionArn": arn, "Input": data},
     )
 
-    sfn_client = local_step_function_client(region)
+    sfn_client = local.step_function_client(region)
     return sfn_client.start_execution(stateMachineArn=arn, name=name, input=data)
 
 
 def generate_execution_name(task_playload: TaskPayload) -> str:
+    """
+    Generate a unique name for the execution.
+
+    This will create a name based on deployment details and the current time.
+
+    It will concatenate the following fields:
+
+    - Task
+    - Portfolio
+    - App
+    - BranchShortName
+    - Build
+    - Current time in seconds
+
+    The ressult will be, for example:  ``deploy-portfolio-app-branch-build-1234567890``
+
+    Args:
+        task_playload (TaskPayload): The task paload to generate the name for
+
+    Returns:
+        str: The name of the execution
+    """
     dd = task_playload.DeploymentDetails
     return "-".join(
         [
